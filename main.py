7a449d2ec2184750e375243a3ccbe0010e99c080
Kiim <kms545487@gmail.com>
2025-09-03 21:06:08 +0900

    for deploy_03 (no-input in backend)

--- main.py
+++ main.py
@@ -1,45 +1,25 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from pydantic import BaseModel
+from ticker_crawler import TickerCrawler, to_excel, to_csv
 import asyncio
-import os
-
-from starlette.middleware.cors import CORSMiddleware
-
-from ticker_crawler import TickerCrawler, to_excel
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://localhost:5500",
-    "http://localhost:8080",
-    "https://Kiim-Miin-Su.github.io",
-]
+class ExportRequest(BaseModel):
+    file_type: str   # "excel" or "csv"
+    filename: str
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.post("/export")
+async def export(req: ExportRequest):
+    async with TickerCrawler() as crawler:
+        await crawler.fetch()
+        data = await crawler.get_ticker_list()
 
+        if req.file_type == "excel":
+            to_excel(data, req.filename)
+        elif req.file_type == "csv":
+            to_csv(data, req.filename)
+        else:
+            return {"error": "Invalid file type"}
 
-class Controller:
-    def __init__(self):
-        self.state = "Initialized"
-
-    async def get_tickers(self, file_name: str = "ticker_list.xlsx"):
-        async with TickerCrawler() as ticker:
-            await ticker.fetch()
-            data = await ticker.get_ticker_list()
-            to_excel(data, file_name)
-            return file_name
-
-controller = Controller()
-
-@app.post("/to-excel")
-async def tickers():
-    file = await controller.get_tickers()
-    return FileResponse(file, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename=os.path.basename(file))
+    return {"message": f"File {req.filename} created successfully."}
